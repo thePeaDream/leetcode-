02fe9d90bcbd3f018e8358abeb4d1aab24b6444c
thePeaDream <PeaDream@outlook.com>
2023-03-10 17:36:11 +0800

    update 拷贝+排序方法

---
+++ Array/合并两个有序数组.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    /*void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)//拷贝+排序 
+    {
+        //把数据拷到nums1尾部
+        for(auto& x: nums2)
+            nums1[m++] = x;
+
+        //排序
+        sort(nums1.begin(),nums1.end());
+    }*/
+
+};
