0f36f928abab9b09614e231126fd7351e8ceac97
thePeaDream <PeaDream@outlook.com>
2023-03-22 23:18:37 +0800

    update 动态规划

--- Array/最大子数组和.cpp
+++ Array/最大子数组和.cpp
@@ -24,4 +24,37 @@ public:
         }
         return max;
     }*/
+    int Max(int a, int b)
+    {
+        return a > b? a: b;
+    }
+
+    int maxSubArray(vector<int>& nums)//动态规划
+    {
+        //把大问题拆分为多个子问题
+        //以nums[0]结尾的最大连续子数组和：放到dp[0] = nums[0]（初始值）
+        //以nums[1]结尾的最大连续子数组和：放到dp[1]
+        //…………
+        //假设全是正数，dp[cur] = dp[cur-1]+nums[cur]
+        //存在负数，若dp[i-1] < 0,  dp[cur] = nums[cur].
+        //所以 dp[cur] = Max(dp[cur-1]+nums[cur], nums[cur]);
+        int ret = nums[0];
+
+        vector<int> dp;
+        dp.resize(nums.size());
+        dp[0] = nums[0];
+
+        for(int cur = 1; cur < nums.size();cur++)
+        {
+            //看dp[cur-1]是否是负数
+            dp[cur] = Max(dp[cur-1]+nums[cur],nums[cur]);
+
+            //在填dp数组的同时选最大值
+            if(dp[cur] > ret)
+                ret = dp[cur];
+        }
+
+        return ret;
+
+    }
 };
