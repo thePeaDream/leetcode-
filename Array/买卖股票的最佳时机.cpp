aa0b1781d30949dc075bf55dbbb6363ca28b4619
thePeaDream <PeaDream@outlook.com>
2023-03-22 23:26:41 +0800

    update 动态规划

--- Array/买卖股票的最佳时机.cpp
+++ Array/买卖股票的最佳时机.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) //暴力
+   /* int maxProfit(vector<int>& prices) //暴力
     {
         //假设最大利润为0
         int maxP = 0;
@@ -16,5 +16,28 @@ public:
             }
         }
         return maxP;
+    }*/
+    int max(int a, int b)
+    {
+        return a > b? a : b;
+    
+    }
+    int maxProfit(vector<int>& prices)//动态规划
+    {
+        //子问题：在第i天卖出能获得的最大利润
+
+        //遍历同时记录历史上买入的最小值
+        int historyBuyMin = prices[0];
+        //prices[i] 减去 历史上买入的最小值 —— 得到这一天卖出能获得的最大利润
+        int maxPf = 0;
+        for(int todaySale = 1; todaySale < prices.size();todaySale++)
+        {
+            //第todaySale天卖出能获得的最大利润
+            maxPf = max(prices[todaySale]-historyBuyMin, maxPf);
+            //更新历史上买入的最小值
+            if(historyBuyMin > prices[todaySale])
+                historyBuyMin = prices[todaySale];
+        }
+        return maxPf;
     }
 };
