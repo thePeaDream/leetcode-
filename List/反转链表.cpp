1e611fc59344a3fefd11813df4a4cf4afd8a71ec
thePeaDream <PeaDream@outlook.com>
2023-04-04 22:41:45 +0800

    update 直接反转

--- List/反转链表.cpp
+++ List/反转链表.cpp
@@ -17,4 +17,18 @@ public:
         }
         return newHead;
     }
+    //ListNode* reverseList(ListNode* head)//直接反转
+    /*{
+        if(head == nullptr||head->next == nullptr) return head;
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        while(cur)
+        {
+            ListNode* tmp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+    //    return pre;
+    //}*/
 };
